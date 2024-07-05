45a2aed21229606b76a094516c217cced7acdabb
Edenilson Fabiano <20231ITA01GB0011@alunos.ifbaiano.edu.br>
2024-07-05 00:03:40 -0300

    initial commit

---
+++ src/main/java/br/com/edenilson/urna/model/Candidato.java
@@ -0,0 +1,17 @@
+package br.com.edenilson.urna.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Candidato {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private int votos;
+}
---
+++ src/test/java/br/com/edenilson/urna/UrnaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.edenilson.urna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
